ad339b5cfa1ce94a29d354f5593b09d5ce91e765
azero <ayoung07275@gmail.com>
2025-09-22 11:19:29 +0900

    DB 저장 코드

--- backend/sync_and_save.sh
+++ backend/sync_and_save.sh
@@ -7,4 +7,4 @@ TODAY_DIR="/var/log/s3-logs/$(date +%Y/%m/%d)/"
 
 aws s3 sync s3://fz-logbucket/AWSLogs/401448503579/vpcflowlogs/ap-northeast-2/$(date +%Y/%m/%d)/ "$TODAY_DIR" --region ap-northeast-2
 
-#python3 app/save_log.py "$TODAY_DIR" >> /var/log/s3-logs/cron.log 2>&1
+python3 app/save_log.py "$TODAY_DIR" >> /var/log/s3-logs/cron.log 2>&1
